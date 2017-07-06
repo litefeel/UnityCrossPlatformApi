9872d84e4089db219874e06328cac1191d08e51b
litefeel <lite3@qq.com>
2017-07-06 17:55:29 +0800

    add document

---
+++ Documentation/DoxygenOutput/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['crossplatformapi',['CrossPlatformAPI',['../classlitefeel_1_1crossplatformapi_1_1_cross_platform_a_p_i.html',1,'litefeel::crossplatformapi']]]
+];
---
+++ Documentation/DoxygenOutput/html/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['platformapi',['PlatformAPI',['../classlitefeel_1_1crossplatformapi_1_1_platform_a_p_i.html',1,'litefeel::crossplatformapi']]]
+];
