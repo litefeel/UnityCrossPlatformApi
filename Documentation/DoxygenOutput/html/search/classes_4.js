e03796f42dd09c13b57549ed9ebbe6f5193c3623
litefeel <lite3@qq.com>
2017-07-26 09:38:07 +0800

    remove unused file

--- Documentation/DoxygenOutput/html/search/classes_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['ui',['UI',['../classlitefeel_1_1crossplatformapi_1_1_u_i.html',1,'litefeel::crossplatformapi']]],
-  ['uiimplandroid',['UIImplAndroid',['../classlitefeel_1_1crossplatformapi_1_1_u_i_impl_android.html',1,'litefeel::crossplatformapi']]],
-  ['uiimpldummy',['UIImplDummy',['../classlitefeel_1_1crossplatformapi_1_1_u_i_impl_dummy.html',1,'litefeel::crossplatformapi']]]
-];
